fe4a79c209033a9c5c1ad666fb9639d4764007cc
Lucía Revaliente Torres <115457404+luciarevaliente@users.noreply.github.com>
2024-05-25 20:31:57 +0200

    python per crear nous csv
    Fitxer python que llegeix el csv d'habitatges i extreu els municipis de la bd

---
+++ tasca4/creacio_bd/municipis.py
@@ -0,0 +1,34 @@
+import csv
+#########################################################################
+# Lectura municipis dels HABITATGES.csv
+HABITATGES = "dades/HABITATGES.csv"  # Nom del fitxer CSV a llegir
+municipis = []  # Llista per emmagatzemar els municipis de la BD
+
+# Obrim i llegim el fitxer CSV
+with open(HABITATGES, mode='r', newline='', encoding='utf-8') as file:
+    csv_reader = csv.DictReader(file)
+
+    for row in csv_reader:
+        # Accedim als valors de cada fila pel nom de la columna
+        municipi = row['Municipi']
+        if municipi != "null" and [municipi] not in municipis:
+            municipis.append([municipi])
+
+        id_llar = row['Id_Llar']
+        any_padro = row['Any_Padro']
+
+        # print(f'Municipi: {municipi}, Id_Llar: {id_llar}, Any_Padro: {any_padro}')
+
+#########################################################################
+# Creació fitxer MUNICIPIS.csv
+MUNICIPIS = "dades/MUNICIPIS.csv"  # Nom del fitxer CSV a crear
+capcalera = ["Municipi"]  # Capçalera de les dades a introduïr
+
+# Escrivim dades, incloent capçaleres
+with open(MUNICIPIS, mode='w', newline='') as new_file:
+    escritor_csv = csv.writer(new_file)
+    escritor_csv.writerow(capcalera)  # Assignem capçaleres
+    for i in municipis:
+        escritor_csv.writerow(i)
+
+print("Arxiu creat!")
